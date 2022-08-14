0c9d9791b1de040e4492835a6c319c02c0fe3e8e
netti-w <106514885+netti-w@users.noreply.github.com>
2022-08-14 17:16:12 +0200

    Add bcrypt middleware function hashing PW and comparing hashed PW against PW in MangoDB

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema( {
   Title: {type: String, required: true},
@@ -26,6 +27,16 @@ let userSchema = mongoose.Schema({
   Birthday: Date,
   FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+// Function hashing submitted password
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+// Function comparing submitted hashed password with hashed password in DB
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
