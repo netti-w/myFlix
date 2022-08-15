51561c6f0142ba2f71dc2adfe2f09f292c1256de
netti-w <106514885+netti-w@users.noreply.github.com>
2022-08-15 15:06:56 +0200

    Update MongoDB connection - testing bug fix

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const Users = Models.User;
 // Connecting LOCAL myFlixDB via Mongoose to perform CRUD operations
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); //test
-// mongoose.connect('mongodb://0.0.0.0:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); //test
+// mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); //test
+mongoose.connect('mongodb://0.0.0.0:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); //test
 
 // Connecting EXTERNAL (MongoDB Atlas) myFlixDB via Mongoose to perform CRUD operations
 // mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
